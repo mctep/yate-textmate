1a48221529e8775ca0e13eadfc5a447e62ada331
mctep <mctep@yandex-team.ru>
2012-11-12 21:10:39 +0400

    fixing some problems

--- Find Match.py
+++ Find Match.py
@@ -11,11 +11,16 @@ class FindMatchCommand(sublime_plugin.TextCommand):
 
 			string = self.view.substr(line).strip()
 
-			m = re.search('^apply (\.[^ ]*) ?(\w+)?', string)
+			m = re.search('^apply ((\.|\/)[^ ]*) ?([0-9a-zA-Z_-]+)?', string)
+
 			if not(m is None):
 				jpath = m.group(1)
-				mode = m.group(2)
-				self.view.window().run_command('show_overlay', {'overlay': 'goto', 'text': '@%s %s' % (jpath, mode)})
+				if jpath == '.':
+					jpath = ''
+				else:
+					jpath = jpath + ' '
+				mode = m.group(3)
+				self.view.window().run_command('show_overlay', {'overlay': 'goto', 'text': '@%s%s' % (jpath, mode)})
 				return
 
 			f = re.search('^apply (\w+)', string)
@@ -23,21 +28,3 @@ class FindMatchCommand(sublime_plugin.TextCommand):
 				func = f.group(1)
 				self.view.window().run_command('show_overlay', {'overlay': 'goto', 'text': '@%s' % func})
 				return
-
-			
-
-class Helper:
-	def __init__(self, string):
-		self.m = re.search('^apply (\.) (mode)', string)
-
-		if self.m is None:
-			self.type = None
-			return None
-
-		g = self.m.group(1);
-		
-		if re.search('^(\.|\/)', g) is None:
-			self.type = 'func'
-		self.type = 'match'
-
-
